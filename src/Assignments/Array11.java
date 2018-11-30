30ce63ffc161020e2e6dd266eaafc5a02d584340
erik8460 <erik8460@ISS2TNRKH2.scdsb.pri>
2018-11-30 14:31:50 -0500

    Assignments Finished

--- src/Assignments/Array11.java
+++ src/Assignments/Array11.java
@@ -1,7 +1,58 @@
 
 package Assignments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class Array11 {
     
+    ArrayList<int[]> testdata; //data being sent in
+    ArrayList<Integer> expected; //expected result
+    String id;
+    
+    
+    Array11(ArrayList<int[]> al,ArrayList<Integer> ex, String name){
+        testdata = al;
+        expected = ex;
+        id = name;
+    }
+    
+    public int solve(int[] nums, int index){
+        if (index >= nums.length) return 0;
+        if (nums[index]==11) return 1 + solve(nums, index+1);
+        else return solve(nums, index+1);
+    }
+    
+    public void test(){
+        System.out.println("Expected\t\t\t\tRun\tResult");
+        System.out.println("=======================================================");
+        for (int i = 0; i < testdata.size(); i++) {
+            String output = id + "(" + Arrays.toString(testdata.get(i)) + ")-> " + expected.get(i);
+            System.out.print(String.format("%-40s", output)); //puts question into 25 even spaces
+            int result = solve(testdata.get(i),0);
+            String status = result==expected.get(i)? "OK": "FAIL";
+            System.out.println(result + "\t" + status);
+        }
+        
+    }
+   
+    public static void main(String args[]){
+        ArrayList<int[]> testing = new ArrayList();
+        ArrayList<Integer> out = new ArrayList();
+        int x[][]={{1,2,11}, {11,11}, {1,2,3,4}, 
+            {1,11,3,11,11}, {11}, {1}, {}, 
+            {11,2,3,4,11,5}, {11,5,11}};
+        for (int nums[]: x) {
+            testing.add(nums);
+        }
+        out.add(1); out.add(2); out.add(0); 
+        out.add(3); out.add(1); out.add(0); 
+        out.add(0); out.add(2); out.add(2); 
+        
+        //create problem
+        Array11 prob = new Array11 (testing,out, "Array11");
+        //run the test
+        prob.test();
+    }
 }
--- src/Assignments/CountPairs.java
+++ src/Assignments/CountPairs.java
@@ -1,7 +1,57 @@
 
 package Assignments;
 
+import java.util.ArrayList;
+
 
 public class CountPairs {
+    ArrayList<String> testdata; //data being sent in
+    ArrayList<Integer> expected; //expected result
+    String id;
     
+    CountPairs(ArrayList<String> al,ArrayList<Integer> ex, String name){
+        testdata = al;
+        expected = ex;
+        id = name;
+    }
+    
+    public int solve(String str){
+        if (str.length()<3 || str.equals("")) 
+        return 0;
+        if (str.charAt(0)==str.charAt(2)) 
+        return 1 + solve(str.substring(1));
+        else 
+        return solve(str.substring(1));
+    }
+    
+    public void test(){
+        System.out.println("Expected\t\t\tRun\tResult");
+        System.out.println("===============================================");
+        for (int i = 0; i < testdata.size(); i++) {
+            String output = id + "(" + testdata.get(i) + ")-> " + expected.get(i);
+            System.out.print(String.format("%-32s", output)); //puts question into 25 even spaces
+            int result = solve(testdata.get(i));
+            String status = result==expected.get(i)? "OK": "FAIL";
+            System.out.println(result + "\t" + status);
+        }
+    }
+    
+    public static void main(String args[]){
+        ArrayList<String> in = new ArrayList();
+        ArrayList<Integer> out = new ArrayList();
+        //add test data
+        in.add("axa"); in.add("axax"); in.add("axbx");
+        in.add("hi"); in.add("hihih"); in.add("ihihhh");
+        in.add("ihjxhh"); in.add(""); in.add("a");
+        in.add("aa"); in.add("aaa");
+        //add expected results
+        out.add(1); out.add(2); out.add(1); 
+        out.add(0); out.add(3); out.add(3); 
+        out.add(0); out.add(0); out.add(0);  
+        out.add(0); out.add(1);  
+        //create problem
+        CountPairs prob = new CountPairs (in,out, "CountPairs");
+        //run the test
+        prob.test();
+    }
 }
